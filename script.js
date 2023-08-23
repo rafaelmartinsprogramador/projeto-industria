be925c9a005b2c6fb81dcd7764f3fc72f99e7e2b
Rafael da Silva Martins <124901505+rafaelmartinsprogramador@users.noreply.github.com>
2023-08-23 12:07:41 -0300

    comit

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ sr.reveal('.tittle-div-sobre-serviços', { rotate: { x: 80, y: 0, z: 0}, duratio
 sr.reveal('.formulario', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
 
 sr.reveal('#div-cards1', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
-sr.reveal('#div-cards2', { rotate: { x: 80, y: 0, z: 0}, duration: 2200 }); 
-sr.reveal('#div-cards3', { rotate: { x: 80, y: 0, z: 0}, duration: 2400 }); 
-sr.reveal('#div-cards4', { rotate: { x: 80, y: 0, z: 0}, duration: 2600 }); 
-sr.reveal('#div-cards5', { rotate: { x: 80, y: 0, z: 0}, duration: 2800 }); 
-sr.reveal('#div-cards6', { rotate: { x: 80, y: 0, z: 0}, duration: 3000 }); 
-sr.reveal('#div-cards7', { rotate: { x: 80, y: 0, z: 0}, duration: 3200 }); 
+sr.reveal('#div-cards2', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards3', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards4', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards5', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards6', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards7', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
 
 sr.reveal('.div-card-title', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
 
