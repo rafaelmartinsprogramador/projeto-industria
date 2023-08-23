e60f83a03872f6822848c12edfeea22a7441f58b
Rafael da Silva Martins <124901505+rafaelmartinsprogramador@users.noreply.github.com>
2023-08-23 11:54:04 -0300

    comit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 window.sr = ScrollReveal ({ reset: true});
 
-sr.reveal('.tittle-div-sobre-serviços', { rotate: { x:80, y:0, z:0}, duration: 2000 }); 
+sr.reveal('.tittle-div-sobre-serviços', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
 
 sr.reveal('.formulario', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
 
@@ -12,3 +12,7 @@ sr.reveal('#div-cards4', { rotate: { x: 80, y: 0, z: 0}, duration: 2600 });
 sr.reveal('#div-cards5', { rotate: { x: 80, y: 0, z: 0}, duration: 2800 }); 
 sr.reveal('#div-cards6', { rotate: { x: 80, y: 0, z: 0}, duration: 3000 }); 
 sr.reveal('#div-cards7', { rotate: { x: 80, y: 0, z: 0}, duration: 3200 }); 
+
+sr.reveal('.div-card-title', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
+
+sr.reveal('.loc-title', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
