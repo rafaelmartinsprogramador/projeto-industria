cfb020a652095ef744e148dde222e19ae5c74dbd
Rafael da Silva Martins <124901505+rafaelmartinsprogramador@users.noreply.github.com>
2023-08-24 22:02:03 -0300

    comit

--- input.js
+++
@@ -1,7 +0,0 @@
-function onlyNumbers(event) {
-    const key = event.key;
-    // Verifica se a tecla pressionada é um número, ponto ou teclas de controle
-    if (isNaN(key) && key !== '.' && !['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'].includes(key)) {
-      event.preventDefault();
-    }
-  }
