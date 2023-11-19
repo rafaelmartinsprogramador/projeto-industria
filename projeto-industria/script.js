b96abd2e3d5b14edf666b60040fb2715f9234124
Rafael da Silva Martins <124901505+rafaelmartinsprogramador@users.noreply.github.com>
2023-11-19 09:10:14 -0300

    comit

---
+++ projeto-industria/script.js
@@ -0,0 +1,18 @@
+
+window.sr = ScrollReveal ({ reset: true});
+
+sr.reveal('.tittle-div-sobre-serviços', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+
+sr.reveal('.formulario', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+
+sr.reveal('#div-cards1', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
+sr.reveal('#div-cards2', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards3', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards4', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards5', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards6', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+sr.reveal('#div-cards7', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 }); 
+
+sr.reveal('.div-card-title', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
+
+sr.reveal('.loc-title', { rotate: { x: 80, y: 0, z: 0}, duration: 2000 });
